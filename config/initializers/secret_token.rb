7d04a218abdc36f211343a3f4a1b646c339b4f18
Aaron Kromer <aaron.kromer@gmail.com>
2012-10-12 17:07:13 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SumBug::Application.config.secret_token = '40789ecd3ff6082751362664c8f3f933b7cc258c065814ddad5c84d06f0e3ef32882cf5ebe2aa759fdd91a9ca74b29f257ded194d5bb321949706afcc34d30c2'
